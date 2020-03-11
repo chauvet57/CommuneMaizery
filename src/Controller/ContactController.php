d0ae6e61714cd27aec12e513c4e0ec7474d59bd8
david57 <chauvet.david57@gmail.com>
2020-03-11 22:13:15 +0100

    mise en place contact mail

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -5,7 +5,10 @@ namespace App\Controller;
 use Twig\Environment;
 use App\Form\ContactType;
 use App\Form\NewLetterType;
+use Symfony\Component\Mime\Email;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -16,7 +19,7 @@ class ContactController extends AbstractController
      * @Route("/contact", name="contact")
      * @return Response
      */
-    public function index(Request $request): Response
+    public function index(Request $request,MailerInterface $mailer): Response
     {
         $form = $this->createForm(ContactType::class);
         $form-> handleRequest($request);
@@ -24,6 +27,29 @@ class ContactController extends AbstractController
         $formLetter = $this->createForm(NewLetterType::class);
         $formLetter-> handleRequest($request);
 
+        
+
+            if ($form->isSubmitted() && $form->isValid()) {
+
+            $contactFormData = $form->getData();
+
+            $email = (new TemplatedEmail())
+                ->from('hello@example.com')
+                ->to($contactFormData->getEmail())
+                ->subject('')
+                ->text($contactFormData->getNom())
+                ->text($contactFormData->getMessage())
+                ->html('');
+
+                $mailer->send($email);
+
+                return $this->render('pages/contact.html.twig',[
+                    'current_menu' => 'contact',
+                    'form' => $form->createView(),
+                    'formLetter' => $formLetter->createView()
+                ]);
+        
+        }
 
         return $this->render('pages/contact.html.twig',[
             'current_menu' => 'contact',
